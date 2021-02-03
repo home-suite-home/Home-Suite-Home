3b37b0fd748b6dc0d535d305f02a9dd60678ff78
Nathan Moulton <natem8@pop-os.localdomain>
2021-02-03 00:27:03 -0500

    Providing 'working software'for Sprint1

---
+++ source/HelloWorld.py
@@ -0,0 +1,12 @@
+''' 
+Take a screenshot of this script running from your bash terminal
+to prove that you've set up a functioning linux environment!
+
+'''
+
+
+greetingPart1 = "Hello"
+greetingPart2 = "World"
+
+
+print(greetingPart1 + " " + greetingPart2 + "!")
