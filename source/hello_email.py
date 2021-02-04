dc66d8726b29e95892f5f038c76782e35eef1fb4
Matthew Dowdy <dowdyma1@gmail.com>
2021-02-04 00:57:04 -0500

    prevented leakage of sensitive data

--- source/hello_email.py
+++ source/hello_email.py
@@ -8,7 +8,7 @@ for testing purposes:
       following email address:
 
     - recipient email: home.suite.home.test.user@gmail.com
-    - recipient pw:    homeuser
+    - recipient pw:    ********
 '''
 
 
@@ -17,44 +17,56 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-sender_email = "home.suite.home.testing@gmail.com"
-reciever_email = "home.suite.home.test.user@gmail.com"
-password = "homesuite"
+def main():
+    try:
+        fileObj = open("password.txt", "r")
+    except:
+        print("Please add the password.txt file to your machine.")
+        return
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "Home-Suite-Home Confirmation"
-message["from"] = sender_email
-message["to"] = reciever_email
+    sender_email = "home.suite.home.testing@gmail.com"
+    reciever_email = "home.suite.home.test.user@gmail.com"
+    password = fileObj.read().strip('\n')
 
-text = """\
-Congratulations! You have completed the setup and can
-start monitoring your property using the best open-source
-home sensor suite available. """
+    fileObj.close()
 
-html = """\
-<html>
-    <body>
-        <p>Congratulations!<br>
-            You have completed the setup and can start monitoring
-            you're property using <a href="https://github.com/home-suite-home/Home-Suite-Home"> the best open-source home sensor suite available</a>
-        </p>
-    </body>
-</html>
-"""
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Home-Suite-Home Confirmation"
+    message["from"] = sender_email
+    message["to"] = reciever_email
 
-# convert to MIME objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
+    text = """\
+    Congratulations! You have completed the setup and can
+    start monitoring your property using the best open-source
+    home sensor suite available. """
 
-# add poth objects to the MIME multipart message
-# the html part will be attempted first
-message.attach(part1)
-message.attach(part2)
+    html = """\
+    <html>
+        <body>
+            <p>Congratulations!<br>
+                You have completed the setup and can start monitoring
+                you're property using <a href="https://github.com/home-suite-home/Home-Suite-Home"> the best open-source home sensor suite available</a>
+            </p>
+        </body>
+    </html>
+    """
 
-# create secure connection and send the send the email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        sender_email, reciever_email, message.as_string()
-    )
+    # convert to MIME objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # add poth objects to the MIME multipart message
+    # the html part will be attempted first
+    message.attach(part1)
+    message.attach(part2)
+
+    # create secure connection and send the send the email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, reciever_email, message.as_string()
+        )
+    print("Email sent.")
+
+main()
