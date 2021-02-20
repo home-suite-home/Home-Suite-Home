10debfb96d79ff1ccd9f27d043513133d7479638
Matthew Dowdy <dowdyma1@gmail.com>
2021-02-19 23:43:44 -0500

    user's checkbox selection is saved even after server is halted

--- source/email_ui.py
+++ source/email_ui.py
@@ -9,10 +9,42 @@ from Email_Component import Email_Component
 from HTTP_Component.htmlReader_MK_2 import Sensor
 
 
-sys.path.append(".")
-colors = {"background": "343434"}
-app = dash.Dash(__name__ )
+def getStorageCheckmarks():
+    try:
+        fileObj = open("enabled_sensors.txt", "r")
+    except:
+        print("Cannot find enabled_sensors.txt")
+        return []
 
+    outList = fileObj.read().split('\n')
+    fileObj.close()
+
+    return outList
+
+
+def writeStorageCheckmarks(checkmarksList):
+    try:
+        fileObj = open("enabled_sensors.txt", "w")
+    except:
+        print("Cannot write to enabled_sensors.txt")
+
+    l = len(checkmarksList)
+
+    if(l == 0):
+        fileObj.write("")
+        print("Wrote nothing.")
+
+    for i in range(l):
+        if(i != l-1):
+            fileObj.write(checkmarksList[i] + '\n')
+        else:
+            fileObj.write(checkmarksList[i])
+
+    fileObj.close()
+
+
+colors = {"background": "343434"}
+app = dash.Dash(__name__)
 
 app.layout = html.Div(
         style={"backgroundColor": colors["background"]},
@@ -45,7 +77,6 @@ app.layout = html.Div(
                                 "height": "40px",
                                 "borderWidth": "1px",
                             },
-                            multiple=True,
                         ),
                         html.Button("Submit", id="button"),
                         html.Div(id='output-sensor-readings'),
@@ -62,18 +93,20 @@ app.layout = html.Div(
                         html.H3(children='Enabled Sensors'),
                         dcc.Checklist(
                             id='enabled-sensors',
+                            persistence=True,
                             options=[
                                 {'label': 'Temperature', 'value': 'tempSensor'},
                                 {'label': 'Humidity', 'value': 'humidSensor'},
                             ],
-                            value=[], # initially enabled
+                            value=getStorageCheckmarks(), # initially enabled
                             labelStyle={
                                 'display': 'block', 
                                 'textAlign': 'justify',
-                                },
-                        )
+                            },
+                        ),
+                        html.Button("Save Selection", id='save-checkmarks-button')
                     ]
-                )
+                ),
             ]
         )
     ],
@@ -81,48 +114,55 @@ app.layout = html.Div(
 
 
 @app.callback(
-        [
-            Output(component_id="remote-email", component_property="value"),
-            Output('enabled-sensors', 'value'),
-            Output('output-sensor-readings', 'children'),
-        ],
-        [
-            Input(component_id="button", component_property="n_clicks"),
-            Input('enabled-sensors', 'value'),
-        ],
-        [
-            State(component_id="remote-email", component_property="value"),
-        ],
+        Output(component_id="remote-email", component_property="value"),
+        Input(component_id="button", component_property="n_clicks_timestamp"),
+        State(component_id="remote-email", component_property="value"),
 )
-def update_output(n_clicks, enabledSensorsList, remote_email):
-    if(not enabledSensorsList):
-        sensor_readings = dash.no_update
-    else:
-        sensor_readings = ""
-
-    for sensor in enabledSensorsList:
-        if(sensor == "tempSensor"):
-            sensor_readings += "Tempurature: " + str(Sensor("temperature").getSensorValue())
-        if(sensor == "humidSensor"):
-            sensor_readings += "Humidity: " + str(Sensor("humidity").getSensorValue())
-
-        sensor_readings += ' '
-
-
-    if remote_email != "":
+def handle_email(button_timestamp, email):
+    if(button_timestamp != None):
         try:
-            print("Sending a test email to " + remote_email)
-            confirmation = Email_Component(remote_email)
+            print("Sending a test email to " + email)
+            confirmation = Email_Component(email)
 
             print(confirmation.confirmation_email())
         except:
-            print("Error: Unable to send a test email to " + remote_email)
-        
-        return ["", enabledSensorsList, sensor_readings]
-    else:
-        return [dash.no_update, enabledSensorsList, sensor_readings]
+            print("Error: Unable to send a test email to " + email)
 
-    return [dash.no_update, enabledSensorsList, sensor_readings]
+        return ""
+    else:
+        return dash.no_update
+
+
+@app.callback(
+        [   # components that will be modified
+            Output('enabled-sensors', 'value'),
+            Output('output-sensor-readings', 'children'),
+        ],
+        [   # components to listen for to run the callback
+            Input('save-checkmarks-button', 'n_clicks_timestamp'),
+        ],
+        [   # components to get the state of upon callaback
+            State('enabled-sensors', 'value'),
+        ],
+)
+def handle_sensor_toggle(button_timestamp, enabledSensorsList):
+    if(button_timestamp != None):
+        writeStorageCheckmarks(enabledSensorsList)
+
+        sensor_readings = ""
+        if(not enabledSensorsList):
+            sensor_readings = dash.no_update
+
+        for sensor in enabledSensorsList:
+            if(sensor == "tempSensor"):
+                sensor_readings += "Tempurature: " + str(Sensor("temperature").getSensorValue())
+            if(sensor == "humidSensor"):
+                sensor_readings += "Humidity: " + str(Sensor("humidity").getSensorValue())
+            sensor_readings += ' '
+
+        return [enabledSensorsList, sensor_readings]
+    else:
+        return [getStorageCheckmarks(), dash.no_update]
 
 
 if __name__ == "__main__":
