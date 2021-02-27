32395aeb819aa0b6a4a7a719075ddf1c4508af29
AndresGraterol <angels.andres0@gmail.com>
2021-02-26 19:55:59 -0500

    Updated selenium test for updated UI

--- testing/Automated_Testing/test_UIpytest.py
+++ testing/Automated_Testing/test_UIpytest.py
@@ -10,22 +10,57 @@ from time import sleep
 def test_lambdatest_todo_app():
     chrome_driver = webdriver.Chrome()
 
+    # obtaining connection to the dash server and opening chrome 
+    # Note: Dash server must be running in order to obtain connection   
     chrome_driver.get('http://127.0.0.1:8050/')
     chrome_driver.maximize_window()
+    sleep(2)
 
-    # Click a sensor checkbox and save the selection 
-    chrome_driver.find_element_by_id("enabled-sensors").click()
-    chrome_driver.find_element_by_id("save-checkmarks-button").click()
-    sleep(5)
-    #chrome_driver.find_element_by_id("li2").click()
+    # Selecting temperature checkbox
+    chrome_driver.find_element_by_xpath("//*[contains(text(), 'Temperature')]").click()
+    sleep(1)
+    # Selecting humidity checkbox
+    chrome_driver.find_element_by_xpath("//*[contains(text(), 'Humidity')]").click()
+    sleep(1)
+    # Save the selections
+    id_check = "save-checkmarks-button"
+    chrome_driver.find_element_by_id(id_check).click()
+    sleep(8)
+
+    # At this point sensor cards should appear; 
+    # click refresh to see them change
+    # Note: sensorSim_random must be running in order to see these change 
+
+    buttons = chrome_driver.find_elements_by_css_selector("button")
+    print(len(buttons))
+   
+    # Refresh the temperature sensor card
+    # If only one card 
+  
+    if (len(buttons) > 2):
+        print("ATTEMPTING REFRESH - temp\n")
+        buttons[2].click()
+    sleep(4)
+
+    # Refresh the humidity sensor card
+    # Two cards
+    
+    if (len(buttons) > 3):
+       print("ATTEMPTING REFRESH - humid\n")
+       buttons[3].click()
+    sleep(4)
 
     # Populating the email field 
     sample_text = "home.suite.home.test.user@gmail.com"
     email_text_field = chrome_driver.find_element_by_id("remote-email")
     email_text_field.send_keys(sample_text)
-    sleep(5)
-    chrome_driver.find_element_by_id("button").click()
+    sleep(3)
 
+    # Sending the email 
+    id_send = "button"
+    chrome_driver.find_element_by_id(id_send).click()
+
+    # Closing the connection to the server and exiting browser 
     chrome_driver.close()
 
 test_lambdatest_todo_app()
