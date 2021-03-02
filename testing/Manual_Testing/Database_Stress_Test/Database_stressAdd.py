4c2e341151d57fcc6913a537e778a9e4701b173d
wv040 <54603910+wv040@users.noreply.github.com>
2021-03-02 14:53:47 -0800

    Add files via upload

--- testing/Manual_Testing/Database_Stress_Test/Database_stressAdd.py
+++ testing/Manual_Testing/Database_Stress_Test/Database_stressAdd.py
@@ -3,15 +3,17 @@ import Sensors
 
 def main():
 
+    nameArray = "temp_0", "temp_1", "temp_2", "temp_3", "temp_4", "temp_5", "temp_6", "temp_7", "temp_8", "temp_9"
+
     data = Database.Database()
 
     temperatureSensorOne = Sensors.TemperatureSensor(
         "temperature",
         "imperial",
         "http://localhost:8080")
-
-    for i in range(45000):
-        data.SendSensorData(temperatureSensorOne.getSensorValue(), "Outdoor Temperature", "temperature")
+    for sensorName in nameArray:
+        for i in range(100000):
+            data.SendSensorData(temperatureSensorOne.getSensorValue(), sensorName, "temperature")
 
 if __name__ == "__main__":
     main()
