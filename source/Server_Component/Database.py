21fd1f4d62747a847d1e64290b0c8d689d3a346b
NathanM117 <36217116+NathanM117@users.noreply.github.com>
2021-03-04 01:38:53 -0500

    Database.py Updates

--- source/Server_Component/Database.py
+++ source/Server_Component/Database.py
@@ -3,7 +3,7 @@
 
 import pymongo as mongo
 import json
-import time
+from timeKeeper import TimeStamps
 
 
 URL = 'localhost'
@@ -21,13 +21,25 @@ PORT = 27017
 
 # Industrial sensors
 
+# RECENT UPDATES
+#
+# Need sensor name and type to query records
+# Timestamp generator swapped for Wyatt's timeKeeper
+# GetAvg updated to be less expensive
+#
 
 class Database:
 	def __init__(self, url, port):
 		self.url = url
 		self.port = port
 		self.connect_status = False
-		self.client = mongo.MongoClient()
+		try:
+			self.client = mongo.MongoClient(self.url, self.port)
+			self.connect_status = True
+		except Exception as e:
+			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
+		else:
+			self.client = mongo.MongoClient()
 
 	def connect(self):
 		if self.connect_status == False:
@@ -39,20 +51,20 @@ class Database:
 				self.connect_status = False
 		else:
 			print("connection already established")
-	
-	# Populates the database with a single recod given its name, sensor type, and raw value. 
+
+	# Populates the database with a single recod given its name, sensor type, and raw value.
 	def SendSensorData(self, data, name, sensor_type):
 		if self.connect_status == True:
 
 			db = self.client['sensorsdb']
 			collection = db['sensors']
-			t = time.time()
+			ts = TimeStamps().getTimestampString()
 
 			dataobj = {
                             "type": sensor_type,
                             "name": name,
                             "value": data,
-                            "time": t
+                            "time": ts
 			}
 
 			collection.insert_one(dataobj)
@@ -70,8 +82,7 @@ class Database:
 
 			for record in records:
 				report_list.append(record)
-				print(record)
-				
+
 			return report_list
 		else:
 			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
@@ -79,27 +90,29 @@ class Database:
 			return []
 
 	# Produces the average value of a sensor in the database
-	def GetAvgVal(self, name):
+	def GetAvgVal(self, name, sensor_type):
 		if self.connect_status == True:
 			db = self.client['sensorsdb']
 			collection = db['sensors']
-			records = collection.find({'name' : name}, {'_id' : 0, 'time' : 0, 'type' : 0, 'name' : 0})
+			records = collection.find({'name' : name, 'type' : sensor_type}, {'_id' : 0, 'time' : 0, 'type' : 0, 'name' : 0})
 			total = 0
-			num = collection.count_documents({'name' : name})
-			value_list = []
-			
+			num = 0
+
 			for record in records:
-				print(record)
 				total += record['value']
-				
-			sensor_avg = total / num
-				
+				num += 1
+
+			try:
+				sensor_avg = total / num
+			except Exception as e:
+				sensor_avg = 0
+
 			return sensor_avg
 		else:
 			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
 			self.connect()
 			return 0
-	
+
 	# Saves/ Updates sensor config data
 	def SaveConfigData(self, sensor_type, name, address, sub_address, min_threshold, max_threshold, alerts):
 		if self.connect_status == True:
@@ -116,19 +129,19 @@ class Database:
                             "max_threshold" : max_threshold,
                             "alerts": alerts
 			}
-			
+
 			# First, check if there's a record for this sensor...
-			if collection.count_documents({'name' : name}) >= 1:
+			if collection.count_documents({'name' : name, 'type' : sensor_type}) >= 1:
 				# ... and update if there's an existing record
-				collection.replace_one({'name' : name}, dataobj)
+				collection.replace_one({'name' : name, 'type' : sensor_type}, dataobj)
 			else:
 				# Otherwise, insert the record
 				collection.insert_one(dataobj)
 		else:
 			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
 			self.connect()
-			
-	# retrieves all config data		
+
+	# retrieves all config data
 	def GetConfigData(self):
 		if self.connect_status == True:
 			db = self.client['sensorsdb']
@@ -139,43 +152,43 @@ class Database:
 			for record in records:
 				report_list.append(record)
 				print(record)
-				
+
 			return report_list
 		else:
 			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
 			self.connect()
 			return []
-			
+
 	# removes a named sensor's config file
-	def DeleteConfigData(self, name):
+	def DeleteConfigData(self, name, sensor_type):
 		if self.connect_status == True:
 			db = self.client['sensorsdb']
 			collection = db['config']
-			
-			collection.delete_one({"name" : name})
-			print("deleting " + name + " ...")
+
+			collection.delete_one({"name" : name, "type" : sensor_type})
+			print("deleting " + name + " : " + sensor_type + " ...")
 		else:
 			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
 			self.connect()
-	
+
 	# Deletes all config data
 	def ClearConfigData(self):
 		if self.connect_status == True:
 			db = self.client['sensorsdb']
 			collection = db['config']
-			
+
 			collection.delete_many({})
 		else:
 			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
 			self.connect()
-	
-	
+
+
 	# Deletes all records in the database
 	def Clear(self):
 		if self.connect_status == True:
 			db = self.client['sensorsdb']
 			collection = db['sensors']
-			
+
 			collection.delete_many({})
 		else:
 			print("Well that didn't work. Check the database address, and make sure the mongod process is running...")
