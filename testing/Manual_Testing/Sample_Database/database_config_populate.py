52b9f30bfd2683a2eabfd58c67bf9ff9c3df87aa
wv040 <54603910+wv040@users.noreply.github.com>
2021-03-06 15:52:32 -0500

    Updates address feild to exclude 'http://'

--- testing/Manual_Testing/Sample_Database/database_config_populate.py
+++ testing/Manual_Testing/Sample_Database/database_config_populate.py
@@ -22,7 +22,7 @@ def main():
     # data.Clear()
 
     # use data.ClearConfigData() to remove all config data
-    # data.ClearConfigData()
+    data.ClearConfigData()
 
 
     ### New Config Structure ###
@@ -35,33 +35,33 @@ def main():
     ##  Indoor Sensor Cluster   ##
 
     # SHT31
-    data.SaveConfigData("temperature", "Room Temperature", "http://10.0.1.60", "temperature", -10, 50, True)
-    data.SaveConfigData("humidity", "Room Humidity", "http://10.0.1.60", "humidity", 30, 75, True)
+    data.SaveConfigData("temperature", "Room Temperature", "10.0.1.60", "temperature", -10, 50, True)
+    data.SaveConfigData("humidity", "Room Humidity", "10.0.1.60", "humidity", 30, 75, True)
 
     #DS18B20
-    data.SaveConfigData("temperature", "Inside Temperature 1", "http://10.0.1.60", "sensor1", -10, 50, True)
-    data.SaveConfigData("temperature", "Inside Temperature 2", "http://10.0.1.60", "sensor2", -10, 50, True)
-    data.SaveConfigData("temperature", "Inside Temperature 3", "http://10.0.1.60", "sensor3", -10, 50, True)
-    data.SaveConfigData("temperature", "Inside Temperature 4", "http://10.0.1.60", "sensor4", -10, 50, True)
+    data.SaveConfigData("temperature", "Inside Temperature 1", "10.0.1.60", "sensor1", -10, 50, True)
+    data.SaveConfigData("temperature", "Inside Temperature 2", "10.0.1.60", "sensor2", -10, 50, True)
+    data.SaveConfigData("temperature", "Inside Temperature 3", "10.0.1.60", "sensor3", -10, 50, True)
+    data.SaveConfigData("temperature", "Inside Temperature 4", "10.0.1.60", "sensor4", -10, 50, True)
 
     ##  Outdoor Sensor Cluster  ##
 
     # SHT31
-    data.SaveConfigData("temperature", "Back Porch Temperature", "http://10.0.1.64", "temperatureSHT", -10, 50, True)
-    data.SaveConfigData("humidity", "Back Porch Humidity", "http://10.0.1.64", "humiditySHT", -10, 50, True)
+    data.SaveConfigData("temperature", "Back Porch Temperature", "10.0.1.64", "temperatureSHT", -10, 50, True)
+    data.SaveConfigData("humidity", "Back Porch Humidity", "10.0.1.64", "humiditySHT", -10, 50, True)
 
     # DS18B20
-    data.SaveConfigData("temperature", "Outside Temperature 1", "http://10.0.1.64", "sensor1", -10, 50, True)
-    data.SaveConfigData("temperature", "Outside Temperature 2", "http://10.0.1.64", "sensor2", -10, 50, True)
-    data.SaveConfigData("temperature", "Outside Temperature 3", "http://10.0.1.64", "sensor3", -10, 50, True)
-    data.SaveConfigData("temperature", "Outside Temperature 4", "http://10.0.1.64", "sensor4", -10, 50, True)
-    data.SaveConfigData("temperature", "Kegerator Temperature", "http://10.0.1.64", "probe", -5, 40, True)
+    data.SaveConfigData("temperature", "Outside Temperature 1", "10.0.1.64", "sensor1", -10, 50, True)
+    data.SaveConfigData("temperature", "Outside Temperature 2", "10.0.1.64", "sensor2", -10, 50, True)
+    data.SaveConfigData("temperature", "Outside Temperature 3", "10.0.1.64", "sensor3", -10, 50, True)
+    data.SaveConfigData("temperature", "Outside Temperature 4", "10.0.1.64", "sensor4", -10, 50, True)
+    data.SaveConfigData("temperature", "Kegerator Temperature", "10.0.1.64", "probe", -5, 40, True)
 
     # DHT 22
-    data.SaveConfigData("temperature", "DHT One Temperature", "http://10.0.1.64", "dht1temperature", -10, 50, True)
-    data.SaveConfigData("humidity", "DHT One Humidity", "http://10.0.1.64", "dht1humidity", -10, 50, True)
-    data.SaveConfigData("temperature", "DHT Two Temperature", "http://10.0.1.64", "dht2temperature", -10, 50, True)
-    data.SaveConfigData("humidity", "DHT Two Humidity", "http://10.0.1.64", "dht2humidity", -10, 50, True)
+    data.SaveConfigData("temperature", "DHT One Temperature", "10.0.1.64", "dht1temperature", -10, 50, True)
+    data.SaveConfigData("humidity", "DHT One Humidity", "10.0.1.64", "dht1humidity", -10, 50, True)
+    data.SaveConfigData("temperature", "DHT Two Temperature", "10.0.1.64", "dht2temperature", -10, 50, True)
+    data.SaveConfigData("humidity", "DHT Two Humidity", "10.0.1.64", "dht2humidity", -10, 50, True)
 
 
 if __name__ == "__main__":
