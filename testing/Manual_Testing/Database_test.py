cea63ea3f0b5ff0a59f60d32b8fab54fecd9d869
AndresGraterol <angels.andres0@gmail.com>
2021-03-07 17:33:28 -0500

    Added tests for checking the functionality of adding and removing config data to a database collection as well as checking GetRecentSensorData()

--- testing/Manual_Testing/Database_test.py
+++ testing/Manual_Testing/Database_test.py
@@ -3,14 +3,48 @@ import pytest
 from Database import *
 import time
 
+
 def test_db():
-    print('Testing...')
-    URL = 'localhost' 
+    URL = 'localhost'
     PORT = 27017
     dbase = Database(URL, PORT)
     dbase.connect()
-	
-    # Populating the database with sensor data 
+
+    # Testing the configuration functions
+    print("Testing Config Collection...")
+
+    dbase.SaveConfigData("temperature", "temp_1",
+                         "http: // localhost: 8080", "temperature", 10, 60, True)
+    dbase.SaveConfigData("temperature", "temp_2",
+                         "http: // localhost: 8080", "temperature", 10, 60, False)
+    dbase.SaveConfigData("temperature", "temp_3",
+                         "http: // localhost: 8080", "temperature", 10, 60, True)
+    dbase.SaveConfigData("humidity", "humid_1",
+                         "http: // localhost: 8080", "humidity", 30, 50, False)
+    dbase.SaveConfigData("humidity", "humid_2",
+                         "http: // localhost: 8080", "humidity", 30, 50, True)
+    dbase.SaveConfigData("humidity", "humid_3",
+                         "http: // localhost: 8080", "humidity", 30, 50, False)
+
+    # Shows record of added config
+    config_list = dbase.GetConfigData()
+
+    # Deleting these sensor names
+    print()
+    dbase.DeleteConfigData("temp_3", "temperature")
+    dbase.DeleteConfigData("humid_3", "humidity")
+
+    # Ensuring that new records show removal
+    config_list = dbase.GetConfigData()
+
+    # uncomment this whenever you wish to clear the config database:
+    #dbase.ClearConfigData()
+
+    # Now testing sending configured sensor data
+    print()
+    print('Testing Sensors Collection...')
+
+    # Populating the database with sensor data
     dbase.SendSensorData(10.0, "temp_1", "Temp")
     dbase.SendSensorData(20.0, "temp_2", "Temp")
     dbase.SendSensorData(30.0, "temp_3", "Temp")
@@ -23,28 +57,46 @@ def test_db():
     dbase.SendSensorData(10.0, "humid_1", "Humid")
     dbase.SendSensorData(20.0, "humid_2", "Humid")
     dbase.SendSensorData(30.0, "humid_3", "Humid")
-	
-    # Prints all the records of the database 
+
+    # Prints all the records of the database
     test_list = dbase.GetData()
 
-    # Checking temperature sensor data 
-    tempTest_value1 = dbase.GetAvgVal("temp_1")
-    tempTest_value2 = dbase.GetAvgVal("temp_2")
-    tempTest_value3 = dbase.GetAvgVal("temp_3")
+    # Checking temperature sensor data
+    tempTest_value1 = dbase.GetAvgVal("temp_1", "temperature")
+    tempTest_value2 = dbase.GetAvgVal("temp_2", "temperature")
+    tempTest_value3 = dbase.GetAvgVal("temp_3", "temperature")
     print("Average for temp_1: " + str(tempTest_value1))
     print("Average for temp_2: " + str(tempTest_value2))
     print("Average for temp_3: " + str(tempTest_value3))
 
-    # Checking humidity sensor data 
-    humidTest_value1 = dbase.GetAvgVal("humid_1")
-    humidTest_value2 = dbase.GetAvgVal("humid_2")
-    humidTest_value3 = dbase.GetAvgVal("humid_3")
+    # Checking humidity sensor data
+    humidTest_value1 = dbase.GetAvgVal("humid_1", "humidity")
+    humidTest_value2 = dbase.GetAvgVal("humid_2", "humidity")
+    humidTest_value3 = dbase.GetAvgVal("humid_3", "humidity")
     print("Average for humid_1: " + str(humidTest_value1))
     print("Average for humid_2: " + str(humidTest_value2))
     print("Average for humid_3: " + str(humidTest_value3))
+    print()
 
-    # uncomment this whenever you wish to clear the database:
+    # Print timestamp for every second (20)
+    for i in range(20):
+        ts = TimeStamps.getTimestamp(TimeStamps)
+        n = 50 + (i % 5)
+        dbase.SendSensorData(n, "outside_1", "temperature")
+        print(ts)
+        time.sleep(1)
+
+    sensors = dbase.GetRecentSensorData(
+        'outside_1', 'temperature', (5 * 0.00028))
+    print()
+
+    # Show that the last 5 timestamps are obtained using the
+    # GetRecentSensorData function
+    for sensor in sensors:
+        print(sensor)
+
+    # uncomment this whenever you wish to clear the sensors database:
     #dbase.Clear()
 
-test_db()
 
+test_db()
