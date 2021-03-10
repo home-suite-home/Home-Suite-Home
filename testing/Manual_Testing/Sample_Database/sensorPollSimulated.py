7cadb037f7cb8ca5ce7e27ad2435fce13f6d3f90
Matthew Dowdy <dowdyma1@gmail.com>
2021-03-10 13:20:53 -0500

    polling for simulated data

---
+++ testing/Manual_Testing/Sample_Database/sensorPollSimulated.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.insert(1, "../../../source")
+import timeKeeper
+sys.path.insert(1, "../../../source/HTTP_Component")
+import Sensors
+sys.path.insert(1, "../../../source/Server_Component")
+import Database
+from time import sleep
+
+data = Database.Database()
+
+def main():
+    configs = data.GetConfigData()
+    loopCounter = 1;
+
+    while True:
+        print()
+        print()
+        print("=============================")
+        print("LOOP: ", loopCounter)
+        print("=============================")
+        print()
+        for record in configs:
+            print("------------------------------")
+            print()
+            type = record["type"]
+            name = record["name"]
+            print(name)
+            address = record["address"]
+            sub_address = record["sub_address"]
+            min_threshold = record["min_threshold"]
+            max_threshold = record["max_threshold"]
+            sensorValue = Sensors.Sensor(url_plug=sub_address, domain='localhost', port='8080').getSensorValue()
+            print(sensorValue)
+            data.SendSensorData(sensorValue, name, type)
+            print()
+        loopCounter += 1
+        sleep(60)
+
+if __name__ == "__main__":
+    main()
