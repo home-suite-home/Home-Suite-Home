726ee715e2721881e741fa0a1b57a8400ae67d72
wv040 <54603910+wv040@users.noreply.github.com>
2021-03-18 15:25:44 -0400

    Adds light intensity unit conversions

--- source/conversions.py
+++ source/conversions.py
@@ -15,6 +15,8 @@ class Units:
     def convert(self, value):
         if self.type == "temperature":
             return self.__convert_temperature(value)
+        elif self.type == "light":
+            return self.__convert_light(value)
         else:
             return value
 
@@ -24,5 +26,18 @@ class Units:
             degrees_f = (value * (9.0/5.0)) + 32
             degrees_f = round(degrees_f, 2)
             return degrees_f
+        else:
+            return value
+
+
+    def __convert_light(self, value):
+        if self.units == "lumens" or self.units == "lm" or self.units == "lum" or self.units == "imperial":
+            lumens = value * 0.09290304; # constant for conversion
+            lumens = round(lumens, 2)
+            return lumens
+        elif self.units == "foot-candle":
+            foot_candles = value / 10.764
+            foot_candles = round(foot_candles, 2)
+            return foot_candles
         else:
             return value
