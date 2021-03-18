0377dbce32f5b4903bc5b18e31d3fd5624a1947a
wv040 <54603910+wv040@users.noreply.github.com>
2021-03-18 02:18:02 -0400

    Adds email alerts and updates poll rate to retrieve from settings config

--- source/sensor_driver.py
+++ source/sensor_driver.py
@@ -4,13 +4,21 @@ import timeKeeper
 from HTTP_Component.Sensors import Sensor
 from Server_Component.Database import Database
 from EmailComponent.EmailController import EmailController
+from settings import Settings
+from alerts import Alert
 
 
-POLL_RATE = 60
+POLL_RATE_DEFAULT = 60
 
 def main():
     db = Database()
+    config = Settings()
     sensorConfigs = db.getConfigData()
+
+    poll_rate = config.get_int_setting("sensors", "poll_rate")
+
+    if poll_rate <= 0:
+        poll_rate = POLL_RATE_DEFAULT
 
     while True:
         for record in sensorConfigs:
@@ -18,16 +26,17 @@ def main():
             sensorValue = Sensor(url_plug = record["sub_address"], domain = record["address"]).getSensorValue()
             print(record["name"], ": ", sensorValue)
 
-            if sensorValue == "NaN":
-                # TODO: send email alert
-                print("Error recieving sensor data")
-            elif sensorValue > record["max_threshold"] or sensorValue < record["min_threshold"]:
-                # TODO: send email alert
-                print("Sensor value out of tollerance")
+            if record["alerts"] is True:
+                if sensorValue == "NaN":
+                    Alert(record, sensorValue).handleAlert()
+                    print("Error recieving sensor data")
+                elif sensorValue > record["max_threshold"] or sensorValue < record["min_threshold"]:
+                    Alert(record, sensorValue).handleAlert()
+                    print("Sensor value out of tollerance")
 
             db.sendSensorData(sensorValue, record["name"], record["type"])
 
-        sleep(POLL_RATE)
+        sleep(poll_rate)
 
 if __name__ == "__main__":
     main()
