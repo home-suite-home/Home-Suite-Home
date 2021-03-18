f107999ea783f88cb0e1a6cd3ef5f92ef7f02df2
wv040 <54603910+wv040@users.noreply.github.com>
2021-03-18 02:19:21 -0400

    Fixes importing of dependencies

--- testing/Manual_Testing/Sample_Database/users_populate.py
+++ testing/Manual_Testing/Sample_Database/users_populate.py
@@ -1,9 +1,13 @@
 import sys
+sys.path.insert(1, "../../../source")
+import timeKeeper
+sys.path.insert(1, "../../../source/HTTP_Component")
+import Sensors
 sys.path.insert(1, "../../../source/Server_Component")
 import Database
 
 def main():
-    db = Database()
+    db = Database.Database()
     db.saveUser("Test User", "home.suite.home.test.user@gmail.com")
 
 if __name__ == "__main__":
