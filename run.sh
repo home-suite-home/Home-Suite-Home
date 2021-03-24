f8cb7bd0a38a3373d6db6d84dbd23085846a4570
daviejones <shaggy13093@aol.com>
2021-03-23 20:05:30 -0400

    added run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,12 @@
 
 # kill all processes on exit
 trap 'kill $(pgrep -f 'sensor_driver.py');
-       kill $(pgrep -f 'EmailCommandDriver.py');
-       kill $(pgrep -f 'User_Interface.py');
-       exit' INT TERM
+      kill $(pgrep -f 'EmailCommandDriver.py');
+      exit' INT TERM
 
 echo "Starting Home-Suite-Home ..."
 echo "CTRL-C to quit"
 
 python3 source/sensor_driver.py &
 python3 source/EmailCommandDriver.py &
-python3 source/User_Interface.py &
+python3 source/User_Interface.py
