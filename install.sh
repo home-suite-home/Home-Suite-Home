2f406c54cecfe083aa835a3b971f81f9a7cc9063
daviejones <shaggy13093@aol.com>
2021-03-23 22:03:00 -0400

    added install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# update libraries
+sudo apt-get update
+
+# get pip
+sudo apt-get -y install python3-pip
+
+# install mongodb
+sudo apt install -y mongodb
+
+# install python libraries
+pip3 install pymongo
+pip3 install dash
+pip3 install dash_daq
